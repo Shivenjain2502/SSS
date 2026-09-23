e07017667095fe28723f10c7fbc0456be5099fa0
Shivenjain2502 <2shivenjain5@gmail.com>
2026-09-24 02:58:26 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "sss-app-v3";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./qrcode.min.js",
+    "./config.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames
+                        .filter(name => name !== CACHE_NAME)
+                        .map(name => caches.delete(name))
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    const request = event.request;
+
+    if (request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(request)
+                    .then(networkResponse => {
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match("./index.html");
+                    });
+            })
+    );
+});
